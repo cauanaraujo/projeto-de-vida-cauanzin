c9b96e805b8d482440ddfd039c855ecc49222098
araujo_cauan <144920158+cauanaraujo@users.noreply.github.com>
2025-06-17 08:02:03 -0300

    06

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-10-05T00-00-00");
+const tempo0bjetivo1 = new Date("2025-10-05T00-00-00");
 let tempoAtual = new Date();
 
 contadores[0].textContent = tempo0bjetivo1 - tempoAtual;
