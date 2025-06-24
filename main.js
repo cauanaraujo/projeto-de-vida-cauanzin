d0721760411051a25e8160c44d266ac0682f1332
araujo_cauan <144920158+cauanaraujo@users.noreply.github.com>
2025-06-24 08:13:00 -0300

    24

--- main.js
+++ main.js
@@ -16,6 +16,21 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempo0bjetivo1 = new Date("2025-10-05T00:00:00");
-let tempoAtual = new Date();
 
-contadores[0].textContent = tempo0bjetivo1 - tempoAtual;
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+
+function calculaTempo(tempo0bjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = new tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 60;
+
+    return (dias - "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos");
+}
+
