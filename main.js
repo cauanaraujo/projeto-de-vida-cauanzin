682f56d62eece22d3251664b131e5c81e20ba508
araujo_cauan <144920158+cauanaraujo@users.noreply.github.com>
2025-07-01 08:38:17 -0300

    1

--- main.js
+++ main.js
@@ -15,22 +15,43 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2024-12-20T00:00:00");
+const tempoObjetivo2 = new Date("2026-01-02T00:00:00");
+const tempoObjetivo3 = new Date("2025-07-15T00:00:00");
+const tempoObjetivo4 = new Date("2026-12-25T00:00:00");
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
-function calculaTempo(tempoObjetivo) {
+function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
-    let tempoFinal = new tempoObjetivo - tempoAtual;
+    let tempoFinal = tempoObjetivo - tempoAtual;
     let segundos = Math.floor(tempoFinal/1000);
     let minutos = Math.floor(segundos/60);
     let horas = Math.floor(minutos/60);
     let dias = Math.floor(horas/24);
 
-    segundos %=60;
-    minutos %=60;
-    horas %=60;
+segundos %= 60;
+minutos %=60;
+horas %=60;
 
-    return (dias + "dias" + horas + "horas" + minutos + " minutos " + segundos + "segundos"); 
+
+if(tempoFinal>0){
+    return (dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ");
+
+}else{
+    return "Prazo Finalizado!"
+}
 }
 
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length; i++){
+        contadores[i].textContent=calculaTempo(tempos[i]);
+    }
+}
+
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro, 1000);
+}
+
+comecaCronometro();
